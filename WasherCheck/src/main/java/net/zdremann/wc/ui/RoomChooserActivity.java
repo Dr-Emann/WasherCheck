e13d253b732ab6c5a6f83d6bd5d7a1042fe3fe9d
Zachary Dremann <dremann@gmail.com>
2013-08-20 17:26:45 -0400

    Fix wrong string in room chooser

--- WasherCheck/src/main/java/net/zdremann/wc/ui/RoomChooserActivity.java
+++ WasherCheck/src/main/java/net/zdremann/wc/ui/RoomChooserActivity.java
@@ -174,7 +174,7 @@ public class RoomChooserActivity extends InjectingActivity implements RoomChoose
 
     private void updateTitle(@Nullable MachineGrouping grouping) {
         if (grouping == null || grouping.id == ROOT_ID)
-            this.setTitle(R.string.change_room);
+            this.setTitle(R.string.choose_room);
         else
             this.setTitle(grouping.name);
     }
